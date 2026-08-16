defda45e84d0acc32335911cd2bab4dbf96b9495
olegolegka <58632030+olegolegka@users.noreply.github.com>
2026-08-16 12:09:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,33 @@
-import time
+import pygame
+import random
 
-food = 10
-mood = 100
-energy = 100
-leaved = True
-print("Добро пожаловать в Тамагочи")
-print("Это твой питомец")
-name = input("Введите имя для питомца: ")
-last_time = time.time()
+pygame.init()
+A = 500
+size = 25
+x = random.randrange(0,A,size)
+y = random.randrange(0,A,size)
+x_apple = random.randrange(0,A,size)
+y_apple = random.randrange(0,A,size)
+screen = pygame.display.set_mode((A,A))
+pygame.display.set_caption("Змейка")
+FPS = 3
 
-while leaved == True:
-    current_time = time.time()
-    if current_time - last_time >= 60:
-        minutes = (current_time - last_time) // 60
-        print("Прошло",minutes,"минут, питомец проголодался и устал")
-        food += 10*minutes
-        mood -= 10*minutes
-        energy -= 15*minutes
-        last_time = current_time
-    print("Состояние питомца:")
-    print("Голод:",food)
-    print("Настроение:",mood)
-    print("Энергия:",energy)
-    print("\\n--- Меню ---")
-    print("1. Покормить (+30 сытости, +10 настроения)")
-    print("2. Поиграть (+20 настроения, -20 энергии)")
-    print("3. Уложить спать (Энергия +100, Голод +10)")
-    print("4. Проверить состояние")
-    choice = input("Выбери действие (1-4): ")
-    if choice == "1":
-        print("Ты покормил питомца")
-        food = max(0,food - 30)
-        mood = min(100,mood + 10)
-    elif choice == "2":
-        if energy > 0:
-            print("Вы поиграли с питомцем")
-            mood = min(100,mood+20)
-            energy = max(0,energy - 20)
-        else:
-            print("Питомец слишком устал для игр")
-    elif choice == "3":
-        print("Питомец поспал")
-        energy  = 100
-        food = max(0,food + 10)
-    elif choice == "4":
-        print("Состояние питомца:")
-        print("Голод:",food)
-        print("Настроение:",mood)
-        print("Энергия:",energy)
-    else:
-        print("Такого действия нет")
-    if mood <= 0 or food >= 100:
-        print("Питомец сбежал")
-        leaved = False
-        time.sleep(2)
-print("Спасибо за игру")
+clock = pygame.time.Clock()
+snake = [(x,y)]
+
+while True:
+  clock.tick(FPS)
+  screen.fill('BLACK')
+  for i,j in snake:
+       pygame.draw.rect(screen,"green",(i,j,size,size))
+
+  pygame.draw.rect(screen,"red",(x_apple,y_apple,size,size))
+
+  pygame.display.update()
+  key = pygame.key.get_pressed()
+  if key[pygame.K_UP]:
+       pass # ВОТ ТУТ ОСТАНОВИЛИСЬ!!!!!!!!! TO DO!!!!!!
+  
+  for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              pygame.quit()
