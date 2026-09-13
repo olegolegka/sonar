b9f492550366f5ea51c63af23d271a643984771d
olegolegka <58632030+olegolegka@users.noreply.github.com>
2026-09-13 14:38:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-local event = game.ReplicatedStorage.ShootEvent
-local bullet = game.ReplicatedStorage.Bullet
-local speed = 50
-local debris = game:GetService("Debris")
-
-event.OnServerEvent:Connect(function(player,mousepos,muzzelpos)
-	local bulletclone = bullet:Clone()
-	bulletclone.Parent = workspace
-	bulletclone.CFrame = CFrame.lookAt(muzzelpos,mousepos)
-	local direction = (mousepos - muzzelpos).Unit
-	local velocity = bulletclone.LinearVelocity
-	velocity.VectorVelocity = direction * speed
-	bulletclone.Touched:Connect(function(hit)
-		if hit.Parent:FindFirstChild("Humanoid") then
-			hit.Parent:FindFirstChild("Humanoid"):TakeDamage(25)
-		end
-		bulletclone:Destroy()
-	end)
-	debris:AddItem(bulletclone,5)
-end)
+local npc= workspace.Noob
+local humanoid = npc.Humanoid
+local pointA=workspace.part1
+local pointB=workspace.part2
+local pointC=workspace.part3
+local pointD=workspace.part4
+local pointE=workspace.part5
+while true do
+	humanoid:MoveTo(pointA.Position)
+	humanoid.MoveToFinished:Wait()
+	humanoid:MoveTo(pointB.Position)
+	humanoid.MoveToFinished:Wait()
+	humanoid:MoveTo(pointC.Position)
+	humanoid.MoveToFinished:Wait()
+	humanoid:MoveTo(pointD.Position)
+	humanoid.MoveToFinished:Wait()
+	humanoid:MoveTo(pointE.Position)
+	humanoid.MoveToFinished:Wait()
+	wait(5)
+end
